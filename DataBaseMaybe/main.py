a12772e469855f0254f7dd8d5aef8f3a6124303c
KravchenkoKir <nkravcenko2@edu.riga.lv>
2023-02-13 17:01:20 +0200

    Creating a DB & trying to make tables

---
+++ DataBaseMaybe/main.py
@@ -0,0 +1,33 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+create_connection(r"C:\Users\Nika\Kir 4.0\School\12.a-Programm-sana-II\DataBaseMaybe\pythonsqlite.db")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""
+execute_query(connection, create_users_table)
